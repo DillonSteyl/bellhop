e36bf94820d18899ce8accd505a9e29c4f585868
Dillon Steyl <dillon.steyl@snazzyshrimp.com>
2024-06-06 20:11:14 +1000

    feat: dockerised localstack and python container
    mainly useful for local dev experiments right now

--- bellhop/src/utils/db.py
+++ bellhop/src/utils/db.py
@@ -1,8 +1,15 @@
 import os
 import boto3
 
-TABLE_NAME = os.environ.get("TABLE_NAME")
+TABLE_NAME = os.environ.get("TABLE_NAME", "WebsocketConnections")
+DEPLOYED_ENVIRONMENT = os.environ.get("DEPLOYED_ENVIRONMENT", "local")
 
 
 def get_db():
+    if DEPLOYED_ENVIRONMENT == "local":
+        return boto3.client(
+            "dynamodb",
+            endpoint_url="http://localstack:4566",
+        )
+
     return boto3.client("dynamodb")
